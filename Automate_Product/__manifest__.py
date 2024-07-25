fb1616b9be1815bd8e3799393399ec52e115dc45
Mohamed Saied <143230022+MohamedSaiedd52@users.noreply.github.com>
2024-07-26 02:14:39 +0300

    Added icon, updated manifest, and removed automate.svg.

--- Automate_Product/__manifest__.py
+++ Automate_Product/__manifest__.py
@@ -1,8 +1,19 @@
 {
     'name': 'Auto-Set PO Line Products',
-    'version': '17.0',
+     'version': '17.0.1.0.0',
     'category': 'Purchases',
-    'summary': 'Custom Module to Manage Vendor Products in Purchase Orders',
+  'summary': 'Automatically create purchase orders and fetch related products when selecting a vendor.',
+    'description': """
+    Auto Purchase Addon
+    ===================
+    This module automates the creation of purchase orders and fetches all related products when a vendor is selected.
+    
+    Features:
+    - Automatically create purchase orders based on stock levels.
+    - Fetch and add all related products to the purchase order when a vendor is selected.
+    - Configurable stock thresholds.
+    - Easy integration with existing purchase workflows.
+    """,
     'depends': ['purchase', 'product'],
     'data': [
         'views/purchase_order_view.xml',
@@ -11,5 +22,5 @@
     'application': False,
     'license': 'LGPL-3',
     'author': 'Mohamed Saied',
-    'icon': 'automate.svg', 
+    'images': ['static/description/icon.png']
 }
