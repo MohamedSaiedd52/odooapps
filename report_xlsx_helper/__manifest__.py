f234aa3133f40b24769a64bd7022bbbf7708fe19
محمد سعيد. محمد على <mohamedatya012120@gmail.com>
2025-01-06 16:25:54 +0200

    ADD stock card report

---
+++ report_xlsx_helper/__manifest__.py
@@ -0,0 +1,11 @@
+
+
+{
+    "name": "Report xlsx helpers",
+    "author": "Mohamed Saied",
+    "category": "Reporting",
+    "license": "AGPL-3",
+    "depends": ["report_xlsx"],
+    "development_status": "Mature",
+    "installable": True,
+}
