cfff14796cf8756e83a623cd4e4c804ed87ce79a
محمد سعيد. محمد على <mohamedatya012120@gmail.com>
2025-01-06 16:38:12 +0200

    ADD stock card report

--- stock_card_report/__init__.py
+++
@@ -1,4 +0,0 @@
-from . import wizard
-from . import reports
-from . import models
-from . import controllers
--- stock_card_report/reports/__init__.py
+++
@@ -1,8 +0,0 @@
-# Copyright 2019 Ecosoft Co., Ltd. (http://ecosoft.co.th)
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-from . import report_xlsx_abstract
-from . import report_xlsx_format
-from . import report_partner_xlsx
-from . import stock_card_report
-from . import stock_card_report_xlsx
-
