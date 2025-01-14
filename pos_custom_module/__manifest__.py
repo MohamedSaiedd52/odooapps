e0f775c18507523f4389eb7050f0577c503b4adf
محمد سعيد. محمد على <mohamedatya012120@gmail.com>
2025-01-14 11:46:13 +0200

    add convert to paid to invoices 1

---
+++ pos_custom_module/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': 'POS Custom Module',
+    'version': '1.0',
+    'summary': 'Adds a button to convert paid POS orders to invoices.',
+    'description': 'This module adds a custom button to convert paid POS orders to invoices and post them.',
+    'version': '1',
+    'author': 'Mohamed Saied',
+    'category': 'Point of Sale',
+    'depends': ['point_of_sale'],
+    'data': [
+        'views/pos_order_views.xml',
+    ],
+    'price': 20.00,
+    'currency': 'USD',
+    'installable': True,
+    'application': False,
+    'images': ['static/description/banner.png'],
+    'license': 'LGPL-3',
+}
