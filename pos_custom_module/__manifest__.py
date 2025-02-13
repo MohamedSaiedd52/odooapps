605b9b000e5f5cf4127dcd2073987a6ebf6f0631
محمد سعيد. محمد على <mohammedsaid@amh.ly>
2025-02-13 14:04:14 +0200

    update

--- pos_custom_module/__manifest__.py
+++ pos_custom_module/__manifest__.py
@@ -10,8 +10,6 @@
     'data': [
         'views/pos_order_views.xml',
     ],
-    'price': 20.00,
-    'currency': 'USD',
     'installable': True,
     'application': False,
     'images': ['static/description/banner.png'],
