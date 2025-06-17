fe660f8df5c27696501f4ede5273e14bffa55268
MohamedSaiedd52 <MohamedSaiedd52>
2025-06-17 12:32:27 +0200

    LOG

---
+++ stock_card_report_aa/report_xlsx_helper_aa/__manifest__.py
@@ -0,0 +1,12 @@
+
+
+{
+    "name": "Report xlsx helpers",
+    "author": "Mohamed Saied",
+    "category": "Reporting",
+    "license": "LGPL-3",
+    "depends": ["report_xlsx_aa"],
+    "development_status": "Mature",
+    "installable": True,
+    'application': False,  
+}
