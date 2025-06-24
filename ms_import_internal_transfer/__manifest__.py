9de66932bb316ba49089835629b2a0ce2043defd
MohamedSaiedd52 <MohamedSaiedd52>
2025-06-24 16:59:23 +0200

    su

--- ms_import_internal_transfer/__manifest__.py
+++ ms_import_internal_transfer/__manifest__.py
@@ -22,5 +22,5 @@
     "application": True,
     "auto_install": False,
     "maintainer": "Mohamed Saied",
-    "images": ["static/description/thumbnail.png"]
+    "images": ["static/description/icon.png"]
 }
