e04f88d9baf3fd3d4495da038e5b8a65c52fb8ad
MohamedSaiedd52 <mohamedatya012120@gmail.com>
2025-06-26 23:38:05 +0200

    + Up

--- stock_picking_type/__manifest__.py
+++ stock_picking_type/__manifest__.py
@@ -1,17 +1,29 @@
 {
-    'name': 'User Location Permission',
+    'name': 'Stock Location Restriction by User - Warehouse Security',
     'summary': 'Restrict stock locations visible to each user based on configuration',
-    'description': """
-        User Location Permission
-        ========================
-        This module allows restricting the stock locations that each user can access or view in warehouse operations.
-        It helps in organizing warehouse security by allowing each storekeeper to interact only with their assigned locations.
-            """,
+   'description': """
+Stock Location Restriction for Users
+====================================
+
+This Odoo module allows you to **restrict warehouse stock locations** visible to each user.
+
+### Key Features:
+- Assign allowed stock locations per user
+- Automatically hide unauthorized warehouse locations
+- Improve security and control in multi-warehouse environments
+- Supports Inventory, Incoming/Outgoing transfers, and more
+
+Ideal for businesses with **multiple warehouses** or branches, where each **storekeeper** should only access their assigned stock locations.
+
+**Compatible with Odoo Enterprise & Community editions.**
+""",
+
     'author': 'Mohamed Saied',
     'maintainer': 'Mohamed Saied <mohamedsaiedd53@gmail.com>',
     'license': 'LGPL-3',
     'version': '1.0',
     'price': 13.99,
+    'keywords': ['stock', 'warehouse', 'location', 'restriction', 'user access', 'security', 'multi warehouse'],
     'currency': 'USD',
     'depends': ['stock','stock_sms'],
     'data': [
