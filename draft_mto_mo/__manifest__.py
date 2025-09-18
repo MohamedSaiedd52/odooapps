909ba68b71f93e5c81685b0eeaa3b7e1a8250b73
MohamedSaiedd52 <mohamedatya012120@gmail.com>
2025-09-18 13:59:46 +0200

    v1

---
+++ draft_mto_mo/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Draft MTO MO",
+    "summary": "Manufacture Orders from MTO stay in Draft state until manually confirmed.",
+    "category": "Manufacturing/Manufacturing",
+    "author": "Mohamed Saied",
+    "maintainers": ["Mohamed Saied"],
+    "license": "LGPL-3",
+    "depends": ["mrp", "stock"],
+    "data": [],
+    "images": [
+        'static/description/banner.gif',
+        'static/description/icon.png',
+    ],
+    
+    "installable": True,
+    "application": False,
+}
