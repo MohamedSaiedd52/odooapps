62a99e9613711ca3afb29690c34184e5a4c52139
MohamedSaiedd52 <mohamedatya012120@gmail.com>
2025-09-20 13:55:55 +0200

    v1

--- stock_card_report_aa/__manifest__.py
+++ stock_card_report_aa/__manifest__.py
@@ -4,7 +4,7 @@
     "category": "Inventory/Warehouse",
     "author": "Mohamed Saied",
     "license": "LGPL-3",
-    "depends": ["base", "web", "stock", "report_xlsx"],
+    "depends": ["base", "web", "stock"],
     "data": [
         "security/group.xml",
         "security/ir.model.access.csv",
