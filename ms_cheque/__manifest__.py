7ec827b62c754d9540f88f725995278d8699593d
MohamedSaiedd52 <mohamedatya012120@gmail.com>
2026-01-30 22:09:19 +0200

    v2

--- ms_cheque/__manifest__.py
+++ ms_cheque/__manifest__.py
@@ -10,10 +10,31 @@
         'views/ms_manage.xml',
         'views/reports.xml',
     ],
+    "author": "Mohamed Saied",
+    "website": "https://www.odoo.com/apps/modules/18.0/ms_cheque_management/",
     'installable': True,
     'auto_install': False,
     'license': 'LGPL-3',
     'price': 30.00,
     'currency': 'USD',
     'images': ['static/description/banner.png', 'static/description/screenshot_incoming.png'],
+       "summary": "Advanced Cheque Management for Odoo 18",
+    "description": """
+MS Cheque Management is a professional Odoo 18 module for managing incoming and outgoing cheques.
+
+Key Features:
+- Incoming & Outgoing Cheque Management
+- Full Accounting Automation
+- Multi-company Support
+- Audit-safe Workflow
+- Bilingual Interface (Arabic / English)
+- Odoo 18 Compatible
+
+Keywords:
+Odoo Cheque Management
+Odoo Cheques
+Odoo Accounting Automation
+Post Dated Cheques Odoo
+Bank Cheques Odoo
+    """,
 }
