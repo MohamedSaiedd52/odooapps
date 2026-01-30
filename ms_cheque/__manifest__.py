bd5213266233c75dd5e7941aab0e9d92e4bd3dcc
MohamedSaiedd52 <mohamedatya012120@gmail.com>
2026-01-30 22:12:49 +0200

    v2

--- ms_cheque/__manifest__.py
+++ ms_cheque/__manifest__.py
@@ -1,26 +1,29 @@
 {
     'name': 'MS Management Cheque',
-    'version': '2.0',
     'author': 'Mohamed Saied',
     'category': 'Accounting',
-    'depends': ['base', 'account','mail','account_accountant'],
+    'depends': ['base', 'account', 'mail'],
     'data': [
         'security/ir.model.access.csv',
         'data/seq_data.xml',
         'views/ms_manage.xml',
         'views/reports.xml',
     ],
-    "author": "Mohamed Saied",
-    "website": "https://apps.odoo.com/apps/modules/18.0/ms_cheque",
+    'website': 'https://apps.odoo.com/apps/modules/18.0/ms_cheque',
     'installable': True,
     'auto_install': False,
     'license': 'LGPL-3',
     'price': 30.00,
     'currency': 'USD',
-    'images': ['static/description/banner.png', 'static/description/screenshot_incoming.png'],
-       "summary": "Advanced Cheque Management for Odoo 18",
-    "description": """
+    'images': [
+        'static/description/banner.png',
+        'static/description/screenshot_incoming.png'
+    ],
+    'summary': 'Advanced Cheque Management for Odoo 18',
+    'description': """
 MS Cheque Management is a professional Odoo 18 module for managing incoming and outgoing cheques.
+
+This module is fully compatible with Odoo Community and Enterprise editions.
 
 Key Features:
 - Incoming & Outgoing Cheque Management
@@ -29,12 +32,5 @@ Key Features:
 - Audit-safe Workflow
 - Bilingual Interface (Arabic / English)
 - Odoo 18 Compatible
-
-Keywords:
-Odoo Cheque Management
-Odoo Cheques
-Odoo Accounting Automation
-Post Dated Cheques Odoo
-Bank Cheques Odoo
     """,
 }
